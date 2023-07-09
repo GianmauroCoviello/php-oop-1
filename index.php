32d2d920f239107df56f26601a634044dcdefca5
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-07-10 00:58:34 +0200

    struttura del boilerplate

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+    // qui verranno inseriti gli oggetti con le instanze
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
